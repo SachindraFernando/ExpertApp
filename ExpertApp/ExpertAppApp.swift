2a3c3cd982fa95a5a99d7ee220427d743e1138ab
Sachindra Fernando <sachindrac.emedia@gmail.com>
2022-11-01 12:17:20 +0530

    Initial Commit

---
+++ ExpertApp/ExpertAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpertAppApp.swift
+//  ExpertApp
+//
+//  Created by Sachindra Fernando on 2022-11-01.
+//
+
+import SwiftUI
+
+@main
+struct ExpertAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
